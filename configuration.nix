d592eacd82a7cb9b06ce7311717c0d0fbe6d6dfa
System administrator <danonator821@gmail.com>
2026-09-15 19:39:21 -0400

    spicetify hopefully x5

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,9 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, pkgs, ... }:
+# { config, pkgs, ... }:
+
+{ inputs, pkgs, ... }:
 
 {
   imports =
